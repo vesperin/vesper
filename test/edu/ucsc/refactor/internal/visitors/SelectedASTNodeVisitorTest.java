8eddefbf4253278b6b39582203f70cf3f9eef0dc
Huascar Sanchez <huascar.sanchez@gmail.com>
2013-12-18 12:03:15 -0800

    cleaned unused imports from test

--- test/edu/ucsc/refactor/internal/visitors/SelectedASTNodeVisitorTest.java
+++ test/edu/ucsc/refactor/internal/visitors/SelectedASTNodeVisitorTest.java
@@ -13,7 +13,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
