859740922e92a5598bd7d033e085fbe766316cd9
Huascar Sanchez <huascar.sanchez@gmail.com>
2014-01-27 11:14:35 -0800

    added more tests for SelectedStatementNodesVisitor.

--- test/edu/ucsc/refactor/internal/visitors/SelectedStatementNodesVisitorTest.java
+++ test/edu/ucsc/refactor/internal/visitors/SelectedStatementNodesVisitorTest.java
@@ -3,16 +3,18 @@ package edu.ucsc.refactor.internal.visitors;
 import edu.ucsc.refactor.Context;
 import edu.ucsc.refactor.Location;
 import edu.ucsc.refactor.Source;
-import edu.ucsc.refactor.SourceSelection;
 import edu.ucsc.refactor.internal.EclipseJavaParser;
 import edu.ucsc.refactor.internal.InternalUtil;
 import edu.ucsc.refactor.internal.SourceLocation;
 import edu.ucsc.refactor.spi.JavaParser;
+import edu.ucsc.refactor.util.Locations;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -32,7 +34,6 @@ public class SelectedStatementNodesVisitorTest {
         parser.parseJava(context);
 
         final Location name = InternalUtil.locateWord(code, "Name");
-        context.setScope(new SourceSelection(name));
 
         final Location invalid = SourceLocation.createLocation(name.getSource(), name.getSource().getContents(), 6, 12);
 
@@ -50,7 +51,6 @@ public class SelectedStatementNodesVisitorTest {
         parser.parseJava(context);
 
         final Location name = InternalUtil.locateWord(code, "Name");
-        context.setScope(new SourceSelection(name));
 
         final SelectedStatementNodesVisitor statements = new SelectedStatementNodesVisitor(name, true);
         context.accept(statements);
@@ -58,6 +58,66 @@ public class SelectedStatementNodesVisitorTest {
 
         assertThat(statements.isSelectionCoveringValidStatements(), is(true));
     }
+
+
+    @Test public void testValidWholeMethodSelection(){
+        final Source code    = InternalUtil.createSourceWithMagicNumber();
+        final Context context = new Context(code);
+
+        parser.parseJava(context);
+
+        final Location boom = InternalUtil.locateWord(code, "boom");
+
+        final SelectedASTNodeVisitor visitor = new SelectedASTNodeVisitor(boom);
+        context.accept(visitor);
+
+        final ASTNode boomNode =  visitor.getMatchedNode();
+
+        assertNotNull(boomNode);
+
+        final Location boomLocation = Locations.locate(boomNode);
+
+        final SelectedStatementNodesVisitor statements = new SelectedStatementNodesVisitor(boomLocation, true);
+        context.accept(statements);
+        statements.checkIfSelectionCoversValidStatements();
+
+        assertThat(statements.isSelectionCoveringValidStatements(), is(true));
+
+    }
+
+
+    @Test public void testInvalidWholeMethodSelection(){
+        final Source code    = InternalUtil.createSourceWithMagicNumber();
+        final Context context = new Context(code);
+
+        parser.parseJava(context);
+
+        final Location boom = InternalUtil.locateWord(code, "boom");
+
+        final SelectedASTNodeVisitor visitor = new SelectedASTNodeVisitor(boom);
+        context.accept(visitor);
+
+        final ASTNode boomNode =  visitor.getMatchedNode();
+
+        assertNotNull(boomNode);
+
+        final Location boomLocation = Locations.locate(boomNode);
+
+        final Location invalid = SourceLocation.createLocation(
+                boomLocation.getSource(),
+                boomLocation.getSource().getContents(),
+                6, /*starts at N*/
+                boomLocation.getEnd().getOffset()
+        );
+
+        final SelectedStatementNodesVisitor statements = new SelectedStatementNodesVisitor(invalid, true);
+        context.accept(statements);
+        statements.checkIfSelectionCoversValidStatements();
+
+        assertThat(statements.isSelectionCoveringValidStatements(), is(false));
+
+    }
+
 
     @After public void tearDown() throws Exception {
         parser  = null;
