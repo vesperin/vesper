59013748b05423a3853aad634bcc975c26bbefc2
Huascar Sanchez <huascar.sanchez@gmail.com>
2014-02-13 11:52:11 -0800

    added JavaDoc to ResultsVisitor

--- ext/vesper-cli/src/edu/ucsc/refactor/cli/ResultVisitor.java
+++ ext/vesper-cli/src/edu/ucsc/refactor/cli/ResultVisitor.java
@@ -6,13 +6,48 @@ import edu.ucsc.refactor.cli.results.*;
  * @author hsanchez@cs.ucsc.edu (Huascar A. Sanchez)
  */
 public interface ResultVisitor {
+    /**
+     * Visits an info result
+     */
     void visit(InfoResult info);
+
+    /**
+     * Visits a commit summary info result
+     */
     void visit(CommitSummaryInfoResult info);
+
+    /**
+     * Visits an error result
+     */
     void visit(ErrorResult error);
+
+    /**
+     * Visits a unit result
+     */
     void visit(UnitResult unit);
+
+    /**
+     * Visits a program unit locations result
+     */
     void visit(LocationsResult locations);
+
+    /**
+     * Visits a notes result
+     */
     void visit(NotesResult notes);
+
+    /**
+     * Visits an issues result
+     */
     void visit(IssuesResult issues);
+
+    /**
+     * Visits a commit history result
+     */
     void visit(CommitHistoryResult history);
+
+    /**
+     * Visits a source result
+     */
     void visit(SourceResult source);
 }
